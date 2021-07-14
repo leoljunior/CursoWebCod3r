9ae49b60defabb955caf116e99d511624c53388e
Leonardo <juhhinx@gmail.com>
2021-07-14 20:30:54 -0300

    add

--- javascript/exercicios/ex16.js
+++ javascript/exercicios/ex16.js
@@ -2,3 +2,6 @@
 como parâmetros dois valores numéricos e uma string referente à operação e a realize com os valoresnuméricos na ordem que foram inseridos. Por exemplo: calculadora (2, ‘+’, 3). A função efetuará a soma de 2 e
 3. Dica: Os sinais das operações são: ‘+’. ‘-’, ‘*’ e ‘/’. Crie um caso default para operações inválidas.
  */
+
+
+const div = (a, b) => a * b
